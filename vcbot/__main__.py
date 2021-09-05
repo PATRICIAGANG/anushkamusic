13569a454e5c721b72e6b9d9b490cb5b214ddac8
Animeshxd <am2646374@gmail.com>
2021-09-05 15:11:09 +0530

    chats test

--- vcbot/__main__.py
+++ vcbot/__main__.py
@@ -118,9 +118,9 @@ async def stop(event):
         await event.edit("stopped")
  
  
-@bot.on(events.CallbackQuery(pattern="Next|Any"))
+@bot.on(events.CallbackQuery(chats=VAR.ADMINS, pattern="Next|Any"))
 @bot.on(events.NewMessage(from_users=VAR.ADMINS, pattern="/next|/any"))
-@admin
+# @admin
 # @stopifstarted
 async def switch(event):
     
