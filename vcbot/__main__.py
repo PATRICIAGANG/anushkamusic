2f068a8a95d74d8e3b33aab2bac3df965b6e0f8e
Animeshxd <am2646374@gmail.com>
2021-09-06 06:05:18 +0530

    fix

--- vcbot/__main__.py
+++ vcbot/__main__.py
@@ -71,7 +71,9 @@ class Factory:
 
     async def start_video(self, input_, repeat=False, with_audio=True):
         if self.is_connected or self.first_time_v:
-            await self.factory.groupcall.start_video(input_ ,repeat=repeat, with_audio=with_audio)
+            groupcall = self.factory.get_group_call()
+            # await groupcall.stop()
+            await groupcall.start_video(input_ ,repeat=repeat, with_audio=with_audio)
             self.is_running = True
             self.first_time_v = False
         else:
