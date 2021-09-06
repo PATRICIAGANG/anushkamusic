632a44790a2cc5900ac2f7b5bd1d3a20edecf74f
Animeshxd <am2646374@gmail.com>
2021-09-06 05:56:44 +0530

    test stop

--- vcbot/__main__.py
+++ vcbot/__main__.py
@@ -150,7 +150,7 @@ async def uptime(e):
 async def switch(event):
     
     # await groupcall.groupcall.pause_playout()   
-    group_call = factory.get_group_call()
+    # group_call = factory.get_group_call()
     temp = await event.respond("Starting...")
     
     url = False
@@ -194,7 +194,7 @@ async def switch(event):
             try:
                 # await groupcall.start_audio(audio)
                 # await groupcall.start_video(video, with_audio=False)
-                await group_call.start_video(video, True, False)
+                await groupcall.start_video(video, True, False)
             except RuntimeError as e:
                 await temp.edit(f"{e}")
                 return
