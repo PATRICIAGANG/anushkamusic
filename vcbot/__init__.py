ab5999fb63770c3a41a1084dacdd6fb3c0023447
Animeshxd <am2646374@gmail.com>
2021-09-07 16:01:31 +0530

    debug mode

--- vcbot/__init__.py
+++ vcbot/__init__.py
@@ -1,12 +1,12 @@
 import logging
-logging.getLogger('telethon').setLevel(logging.WARNING)
+logging.getLogger('telethon').setLevel(logging.INFO)
 # logging.getLogger('pytgcalls').setLevel(logging.)
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 from .config import Config as VAR
 from datetime import datetime
 
-logging.basicConfig(level=logging.DEBUG, format='[%(funcName)s]:%(levelname)s: %(message)s', filename="app.log")
+logging.basicConfig(level=logging.DEBUG, format='[%(funcName)s]:%(levelname)s: %(message)s')
 
 user = TelegramClient(StringSession(VAR.STRING), VAR.API_ID, VAR.API_HASH).start()
 bot = TelegramClient("bot_sess", VAR.API_ID, VAR.API_HASH).start(bot_token=VAR.BOT_TOKEN)
