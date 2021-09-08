e34660f0c977d60779de2f005c69e1da944c0b16
PATRICIAGANG <85884951+PATRICIAGANG@users.noreply.github.com>
2021-09-08 23:11:21 +0530

    Update __init__.py

--- vcbot/__init__.py
+++ vcbot/__init__.py
@@ -3,7 +3,7 @@ logging.getLogger('telethon').setLevel(logging.INFO)
 # logging.getLogger('pytgcalls').setLevel(logging.)
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-from .config import Config as VAR
+from config import Config as VAR
 from datetime import datetime
 
 logging.basicConfig(level=logging.DEBUG, format='[%(funcName)s]:%(levelname)s: %(message)s')
