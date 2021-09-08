c622613ddfb3e86ea223139ffb9e88007a50a0e2
PATRICIAGANG <85884951+PATRICIAGANG@users.noreply.github.com>
2021-09-08 23:19:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ class Config:
     API_ID: int = 7324596
     API_HASH: str = 'afdbfc67c94075180e4d8a0cd6549e54'
     STRING: str = ""
-    BOT_TOKEN: str = ''
+    BOT_TOKEN: str = '1971865305:AAH3TLHO5HrlKdhmTHV6Xh08t_4Yybl7e4I'
     ADMINS: list = [663149800,]
